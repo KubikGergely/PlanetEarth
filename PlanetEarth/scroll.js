c5674f30ec84e7ce7647005d62cc46d48442f559
KubikGergely <124676056+KubikGergely@users.noreply.github.com>
2025-11-21 10:01:49 +0100

    Delete PlanetEarth directory

--- PlanetEarth/scroll.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-50px";
-  }
-}
